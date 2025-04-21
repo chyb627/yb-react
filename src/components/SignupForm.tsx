1aad7893c076b5a283648792c3441485487cadda
cha <chyb627@naver.com>
2025-04-21 18:56:40 +0900

    firebase login

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -70,20 +70,31 @@ export default function SignupForm() {
       <h1 className="form__title">회원가입</h1>
       <div className="form__block">
         <label htmlFor="email">이메일</label>
-        <input type="email" name="email" id="email" required onChange={onChange} />
+        <input type="email" name="email" id="email" required onChange={onChange} value={email} />
       </div>
 
       <div className="form__block">
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" required onChange={onChange} />
+        <input type="password" name="password" id="password" required onChange={onChange} value={password} />
       </div>
 
       <div className="form__block">
         <label htmlFor="password_confirm">비밀번호 확인</label>
-        <input type="password" name="password_confirm" id="password_confirm" required onChange={onChange} />
+        <input
+          type="password"
+          name="password_confirm"
+          id="password_confirm"
+          required
+          onChange={onChange}
+          value={passwordConfirm}
+        />
       </div>
 
-      {error && error?.length > 0 && <div className="form__block form__error">{error}</div>}
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
 
       <div className="form__block">
         계정이 이미 있으신가요?
